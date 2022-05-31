4d1c6071bd8644df80780b4e044cef8afdfea5fd
Vitaly <krivoruchek1@gmail.com>
2022-05-31 21:02:17 +0300

    Lesson_1 to JS

--- lesson_1.js
+++ lesson_1.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+// console.log("Hello");
 
 let a = 20;
 let b = "30";
@@ -9,12 +9,45 @@ let sum_body = a + +b;
 let sum_body_string = String(a + c);
 
 
-let type1 = typeof(sum_body);
-let type2 = typeof(sum_body_string);
+// let type1 = typeof(sum_body);
+// let type2 = typeof(sum_body_string);
 
 
-console.log("sum_body =", sum_body);
-console.log("type_sum_body =", type1);
+// console.log("sum_body =", sum_body);
+// console.log("type_sum_body =", type1);
 
-console.log("sum_body_string =", sum_body_string);
-console.log("type_sum_body_string =", type2);
+// console.log("sum_body_string =", sum_body_string);
+// console.log("type_sum_body_string =", type2);
+
+
+// let compare_1 = a > b;
+
+// console.log(compare_1);
+// console.log(typeof(compare_1));
+
+// let bool_true = true;
+// let bool_false = false;
+
+// let type_true = typeof(bool_true);
+// console.log(typeof(bool_true));
+
+// let b_1 = bool_true + bool_false
+
+// console.log("============");
+// console.log(b_1);
+// console.log(typeof(b_1));
+
+
+let condition = true;
+
+if (condition) 
+    {
+        let t_1 = 5;
+        let t_2 = 10;
+        let result = t_1 + t_2;
+        console.log("IF_RESULT =", result)
+    }
+    else
+    {
+        console.log("IF_RESULT = false")
+    }
