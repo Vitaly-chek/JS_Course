9a88280645bcffa52c38a62b22152031c5d8d10f
Vitaly <krivoruchek1@gmail.com>
2022-06-03 20:09:36 +0300

    Update file lesson

---
+++ Lesson_2.js
@@ -0,0 +1,32 @@
+let hour = 8;
+
+if (hour < 0 || hour >24)
+{
+    console.log("Error");
+}
+else if (hour < 18 && hour >=8)
+{
+    console.log("Open");
+}
+else if (hour == "Sundey" || hour == "Satuday")
+{
+    console.log("Free day");
+}
+else 
+    console.log("Close") ;
+
+
+
+
+
+
+// for (i=0; i=20; i++)
+// {
+//     i++;
+//     console.log(i);
+// }
+
+// if (1 || 0 || 0)
+// {
+//     console.log("Hello") 
+// }
