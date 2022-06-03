b0da6da6efab6b6cde5904652d27b1260566aea4
Vitaly <krivoruchek1@gmail.com>
2022-06-04 00:27:54 +0300

    ew functional

--- Lesson_2.js
+++ Lesson_2.js
@@ -1,4 +1,5 @@
-let hour = 8;
+// let hour = 8;
+// let day = 4;
 
 // if (hour < 0 || hour >24)
 // {
@@ -15,22 +16,49 @@ let hour = 8;
 // else 
 //     console.log("Close") ;
 
+                        //my code
 
-function workshop (days, hours){
+// function workshop (day)
+// {
+//     if (typeof(day) == "string")
+//     {
+//         if (day == 'Sat' || day == 'Sun')
+//         {
+//             console.log("Close")
+//         }
+//         else
+//         {
+//             workhour(hour)
+//         }
+//     }
+//     else
+//     {
+//         console.log("Error")
+//     }
+// }
 
-    if (typeof(days) == "string" && typeof(hours)  == "number" )
-    {
-        console.log("workday = " + days)
-        console.log("workhours = " + hours)
-    }
 
+// function workhour (hour)
+// {
+//     if (typeof(hour) == "number")
+//     {
+//         if (hour < 8 || hour >= 18)
+//         {
+//             console.log("Close");
+//         }
+//         else
+//         {
+//             console.log("Open");
+//         }
+//     }
+//     else
+//     {
+//         console.log("Error")
+//     }
 
+// }
 
-    // console.log("workday = " + days);
-    // console.log("workhours = " + hours);
-}
-
-workshop("Mondey", 2)
+                    // exit my code
 
 
 // for (i=0; i=20; i++)
@@ -43,3 +71,46 @@ workshop("Mondey", 2)
 // {
 //     console.log("Hello") 
 // }
+
+             //lesson 
+
+// let day_titel = "Sunday1";     
+// let ww_days = ['Monday', 'Tueesday', 'Wednesday', 'Thursday', 'Fridey', 'Saturday', 'Sunday']
+
+// if (!ww_days.includes(day_titel))
+// {
+//     console.log("Error")
+// }
+// else
+// {
+
+// }
+
+            //end
+
+
+
+
+function workshop (day)
+{
+    let h_days = ['Saturday', 'Sunday']
+    let w_days = ['Monday', 'Tueesday', 'Wednesday', 'Thursday', 'Fridey']
+
+    if (typeof(day) == "string")
+    {
+        if (w_days.includes(day))
+        {
+            return true;
+        }
+        else if (h_days.includes(day))
+        {
+            return false;
+        }
+        else
+        {
+            console.log("Error!!");
+        }
+    }
+}
+
+console.log(workshop(""))
